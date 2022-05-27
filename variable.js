bab2cf670b51b9e14b7935721416367c7240f67d
BorisBarrera <87447530+BorisBarrera@users.noreply.github.com>
2022-05-26 21:21:56 -0600

    Add files via upload
    init

---
+++ variable.js
@@ -0,0 +1,20 @@
+let firstName = "Boris"
+let visible = true
+let nothing
+let emptyValue = null
+
+let lastName = "Barrera"
+let maritalStatus = "Single"
+let country = "El Salvador"
+let age = 31
+
+
+console.log(typeof(firstName))
+console.log(typeof(visible))
+console.log(typeof(nothing))
+console.log(typeof(emptyValue))
+console.log(firstName)
+console.log(lastName)
+console.log(maritalStatus)
+console.log(country)
+console.log(age)
